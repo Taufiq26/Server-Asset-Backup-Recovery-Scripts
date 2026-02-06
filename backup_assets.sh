62bc17a6c2515a8dcecac6bc615908bd2f660584
Taufiq Ridwan <taufiq26.ridwan@gmail.com>
2026-02-06 16:00:40 +0700

    Enhance backup script with Google Drive sync and cleanup
    Added functionality to sync assets to Google Drive and delete old files.

--- backup_assets.sh
+++ backup_assets.sh
@@ -17,6 +17,10 @@ mkdir -p "$TARGET_DIR"
 # Using mc mirror to copy the bucket contents to the destination folder
 mc mirror "$SOURCE_ALIAS" "$TARGET_DIR"
 
+# Upload ke Google Drive (Sync folder)
+echo "Syncing assets to Cloud..."
+rclone copy $TARGET_DIR gdrive:backups_project_x/minio_assets/assets_$DATE
+
 if [ $? -eq 0 ]; then
     echo "Backup Assets successfully: $TARGET_DIR"
 else
@@ -29,5 +33,8 @@ fi
 echo "Clean up old assets (keep the 2 newest ones)..."
 # Mencari folder dengan pola 'assets_*' di dalam folder backup
 ls -dt $BACKUP_BASE_DIR/assets_*/ | tail -n +3 | xargs -r rm -rf
+
+# Hapus file di Cloud yang lebih tua dari 7 hari
+rclone delete gdrive:backups_project_x/minio_assets --min-age 7d --rmdirs
 
 echo "Finished."
